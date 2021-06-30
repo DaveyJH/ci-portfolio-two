9c552897421ca8e752bda05098730981c3a3cf55
DaveyJH <daveyj.dh@googlemail.com>
2021-06-30 22:05:03 +0100

    ADD timer function with max time, best time etc [challenge.js]

--- assets/js/challenge.js
+++ assets/js/challenge.js
@@ -23,6 +23,61 @@ let bestScoreHTML = document.getElementsByClassName("best score")[0]; // bestSco
 let bestScore = 0; // keep a record of the best score without having to get from DOM
 let score = 0; // score count
 
+
+// ! timer variables and functions
+
+// credit to Code Institute course content for basic timer function
+// modified with basic solution credit to https://stackoverflow.com/users/854246/joseph-marikle
+// https://stackoverflow.com/questions/8043026/how-to-format-numbers-by-prepending-0-to-single-digit-numbers/8043061#8043061
+// first answer which applies for positive integers only which works for this situation
+// the answer combines a string with a number so extra steps have been taken to convert
+// to strings and numbers where necessary
+
+let intervalCount; // accessible variable
+let seconds = document.getElementById("seconds"); // seconds span // ? which method is preferred? l.42
+let minutes = document.getElementById("minutes"); // minutes span
+let secondsTime = 0; // start value
+let minutesTime = 0; // start value
+let bestSeconds = document.getElementsByClassName("best time")[0].children[1]; // best seconds span // ? which method is preferred? l.36
+let bestMinutes = document.getElementsByClassName("best time")[0].children[0]; // best minutes span
+function timer() {
+  intervalCount = setInterval(function() {
+    secondsTime++; // increment seconds by 1
+    seconds.innerHTML = (("0" + secondsTime).slice(-2)).toString(); // display seconds as 2 digit string
+    minutes.innerHTML = (("0" + minutesTime).slice(-2)).toString(); // display minutes as 2 digit string
+    if (secondsTime === 60) { // do not display 60
+      secondsTime = 0; // reset second count
+      seconds.innerHTML = (("0" + secondsTime).slice(-2)).toString(); // display 00 seconds
+      minutesTime++; // increment minutes by 1
+      minutes.innerHTML = (("0" + minutesTime).slice(-2)).toString(); // display minutes as 2 digit string
+    }
+    if ((secondsTime === 59) && (minutesTime === 59)) { // stop timer at maximum time of 59:59
+      clearInterval(intervalCount);
+    }
+  }, 1000); // 1000ms = 1 second
+}
+
+/** check current completion time against best time,
+ * if quicker => replace best time
+ */
+function checkTime() {
+  let testSeconds = Number(bestSeconds.innerHTML); // convert seconds string to number
+  let testMinutes = Number(bestMinutes.innerHTML); // convert minutes string to number
+  if ((secondsTime <= testSeconds||bestSeconds.innerHTML === "--") // check values of bestSeconds
+  && (minutesTime <= testMinutes||bestMinutes.innerHTML === "--")) { //check values of bestMinutes
+    bestSeconds.innerHTML = (("0" + secondsTime).slice(-2)).toString(); // write bestSeconds
+    bestMinutes.innerHTML = (("0" + minutesTime).slice(-2)).toString(); // write bestMinutes
+  }
+}
+
+/** reset timer to 0 */
+function resetTime() {
+  secondsTime = 0;
+  minutesTime = 0;
+}
+
+// ! setting up the game functions
+
 /** set color of selector-box color balls to available colors */
 function setSelectorBalls() {
   for (let i = 0; i < colorSelectors.length; i++) {
