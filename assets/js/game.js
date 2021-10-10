43600c85f7e6d5b17e04a1a076b0d90395624a52
DaveyJH <daveyj.dh@googlemail.com>
2021-10-10 15:35:12 +0100

    ADD note/issue for best time/score hidden values

--- assets/js/game.js
+++ assets/js/game.js
@@ -869,6 +869,7 @@ function setSolutionBalls() {
   }
 }
 
+// todo add hidden value script #13
 /** check if best score has been set
  * - if it is larger than the current winning
  * score : change the displayed number to the best score
@@ -880,6 +881,7 @@ function checkScore() {
   }
 }
 
+// todo add hidden value script #13
 /** check current completion time against best time
  * - if quicker : replace best time
  */
